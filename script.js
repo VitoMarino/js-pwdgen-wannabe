09d80c20ab0d503f7cdf9e1a77b879979d68c0b3
VitoMarino <marino.vito2001@gmail.com>
2024-05-07 15:51:16 +0200

    js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 //* Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito. Infine scrivi sulla pagina (nomecognomecolorepreferito24) *//
 
 const userName = prompt("Scrivi il tuo nome");
-const userLastName = prompt("Scrivi il tuo cognome");
+const userlastName = prompt("Scrivi il tuo cognome");
 let colorFavourite = prompt("Scrivi il tuo colore preferito");
 
-console.log(userName);
-console.log(userLastName);
-console.log(colorFavourite);
+console.log("Ciao " + userName);
+console.log("Ciao " + userlastName);
+console.log("Che bello che il tuo colore preferito " + colorFavourite);
+
+document.writeln(userName);
+document.writeln(userlastName);
+document.writeln(colorFavourite);
